c2273fad44e483b8d13c610a6232556054456217
Andrii-Vekker <vekker304@gmail.com>
2022-08-24 22:11:10 +0300

    erwer

--- src/fetchphoto.js
+++ src/fetchphoto.js
@@ -1,15 +1,45 @@
 import axios from "axios";
 export { getImg }
-import { name, count } from "./index";
-const URL = `https://pixabay.com/api/`
-const KEY = `29175258-0e972b66084e1db5719a62740`
+import { name, currentPage } from "./index";
+const URL = `https://api.themoviedb.org/3`
+const KEY = `8d439eb5ac7a153643a933bcb130103b`
 
 async function getImg() {
    try {
      const response = await axios.get
-        (`${URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${count}&per_page=40`);
-    return response.data
-   } catch (error) {
-    console.log(error)
+        (`${URL}/search/movie?api_key=${KEY}&language=en-US&query=${name}`);
+    return response
+   }
+   catch (error) {
+   
    }
 };
+// getImg().then(response => console.log(response.data))
+// https://api.themoviedb.org/3/movie/550?api_key=8d439eb5ac7a153643a933bcb130103b
+// ${BASE_URL}?api_key=${API_KEY}&query=${inputValue}&page=${pageNumber}
+
+// async function getPopularImg() {
+//    try {
+//      const response = await axios.get
+//         (`${URL}/trending/all/day?api_key=${KEY}&language=en-US&popular`);
+//     return response
+//    }
+//    catch (error) {
+   
+//    }
+// };
+
+// getPopularImg().then(response => console.log(response))
+
+
+// let name = "cat"
+//  async function fetchSearchingFilms() {
+//        try {
+//       const response = await axios.get(`${URL}/search/movie?api_key=${KEY}&language=en-US&query=${name}`);
+//         return response;
+//     } catch (error) {
+//       console.error('Something wrong! Can not search films' + error);
+//     }
+// } 
+
+// fetchSearchingFilms().then(response =>console.log(response))
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+// import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.min.css';
+// import 'tui-pagination/dist/tui-pagination.css';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+// import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { getImg } from "./fetchphoto";
 export { name, count }
 
-const URL = `https://pixabay.com/api/`;
-const KEY = `29175258-0e972b66084e1db5719a62740`;
+// const URL = `https://api.themoviedb.org/3`;
+// const KEY = `8d439eb5ac7a153643a933bcb130103b`;
 
 let name = "";
 let count = 1;
@@ -20,115 +25,169 @@ const refs = {
 };
 
 
-// const { height: cardHeight } = document
-//   .querySelector(".gallery")
-//   .firstElementChild.getBoundingClientRect().top;
-
-// window.scrollBy({
-//   top: cardHeight * 2,
-//   behavior: "smooth",
-// });
 
 
 
-refs.gallery.addEventListener("click", galleryHandler)
-refs.input.addEventListener("input", inputHandler);
+
+// refs.gallery.addEventListener("click", galleryHandler)
+// refs.input.addEventListener("input", inputHandler);
 refs.form.addEventListener("submit", formHandler);
-refs.loadMoreBtn.addEventListener("click", loadMoreBtnHandler);
+// refs.loadMoreBtn.addEventListener("click", loadMoreBtnHandler);
 
-function galleryHandler(e) {
-  e.preventDefault();
-  const lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250,
-  captionPosition: "bottom",
-  showCounter: true,
-  enableKeyboard: true
-});
-}
+// function galleryHandler(e) {
+//   e.preventDefault();
+//   const lightbox = new SimpleLightbox(".gallery a", {
+//   captionsData: "alt",
+//   captionDelay: 250,
+//   captionPosition: "bottom",
+//   showCounter: true,
+//   enableKeyboard: true
+// });
+// }
 
 
-function loadMoreBtnHandler(e) {
-  e.preventDefault
-  const cardHeight = refs.gallery.getBoundingClientRect().height/2
-  console.log(cardHeight)
+// function loadMoreBtnHandler(e) {
+//   e.preventDefault
  
-  count += 1;
-  window.scrollBy({
-  behavior: "smooth", top: cardHeight, left:0
-});
-  if (name !== "") {
-    getImg().then((photo) =>{
-            if (count > photo.totalHits/40) {
-          Notify.failure('Were sorry, but you ve reached the end of search results');
-              refs.loadMoreBtn.classList.remove("loadMoreVisible");
-       refs.loadMoreBtn.classList.add("loadMoreHidden");
-      };
-      renderGallery(photo)
-    })
-  .catch(error => console.log(error));
-  };
-  return count
-};
+//   if (name !== "") {
+//     getImg().then((photo) =>{
+//             if (count > photo.totalHits/40) {
+//           Notify.failure('Were sorry, but you ve reached the end of search results');
+//               refs.loadMoreBtn.classList.remove("loadMoreVisible");
+//        refs.loadMoreBtn.classList.add("loadMoreHidden");
+//       };
+//       renderGallery(photo)
+//     })
+//   .catch(error => console.log(error));
+//   };
+//   return count
+// };
 
-function inputHandler(e) {
-  if (refs.input.value === "") {
-    refs.galleryContainer.innerHTML = ""
-    refs.loadMoreBtn.classList.add("loadMoreHidden");
-    refs.loadMoreBtn.classList.remove("loadMoreVisible");
-    Notify.failure('Sorry, there are no images matching your search query. Please try again')
-    };
-}
+// function inputHandler(e) {
+//   if (refs.input.value === "") {
+//     // refs.galleryContainer.innerHTML = ""
+//     Notify.failure('Sorry, there are no images matching your search query. Please try again')
+//     };
+// }
+///////////////////////////////////////////////////////
+
+
+
+    
+  
+
+
 
 function formHandler(e) {
   e.preventDefault();
-  refs.loadMoreBtn.classList.add("loadMoreHidden");
   name = refs.input.value.trim();
-   refs.galleryContainer.innerHTML = ""
-  if (!name) {
-    Notify.failure('Sorry, there are no images matching your search query. Please try again');
-     refs.loadMoreBtn.classList.remove("loadMoreVisible");
-    refs.loadMoreBtn.classList.add("loadMoreHidden");
-    };
-    if (name !== "") {
-      refs.loadMoreBtn.classList.add("loadMoreVisible");
-      refs.loadMoreBtn.classList.remove("loadMoreHidden")
-     getImg().then((photo) =>{
-            if (photo.hits.length === 0) {
-          Notify.failure('Were sorry, but you ve reached the end of search results');
-              refs.loadMoreBtn.classList.remove("loadMoreVisible");
-       refs.loadMoreBtn.classList.add("loadMoreHidden");
-      }else Notify.success(`Hooray! We found ${photo.totalHits} images`);
-      renderGallery(photo)
-    }).catch(error => error(console.log(error)));
-    };
-};
+  getImg().then((photo) => {
     
+    renderGallery(photo.data)
+   
+  }).catch(error => error(console.log(error)));
+    
+};
+    const url ="https://image.tmdb.org/t/p/w500"
 function createGallery(array) {
-  return array.hits.reduce((acc, { largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => acc +
-    `<a gallery__item" href="${largeImageURL}">
-  <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+  console.log(array)
+  // const options = {
+  // totalItems: array.total_results,
+  // itemsPerPage: 20,
+  // visiblePages: 20,
+  //   page: 1,
+  //   centerAlign: false,
+  //   firstItemClassName: 'tui-first-child', 
+  //     lastItemClassName: 'tui-last-child'
+  // }
+  
+// const container = document.getElementById('tui-pagination-container');
+// const myPagination = new Pagination(container, options);
+// // instance.getCurrentPage();
+  return array.results.reduce((acc, { original_title, poster_path, backdrop_path }) => acc +
+    `<a gallery__item" href="${original_title}">
+  <img class="gallery__image" src="${url+poster_path}" alt="" loading="lazy"/>
    <div class="info">
      <p class="info-item">
-       <b class="b">Likes:</b><b>${likes}</b>
+       <b class="b">Year</b><b></b>
     </p>
      <p class="info-item">
-       <b>Views:</b><b>${views}</b>
+       <b>Views:</b><b></b>
      </p>
      <p class="info-item">
-       <b>Comments:</b><b>${comments}</b>
+       <b>Comments:</b><b></b>
      </p>
      <p class="info-item">
-       <b>Downloads:</b><b>${downloads}</b>/</p>
+       <b>Downloads:</b><b></b></p>
    </div>
   </a>`, "");
 };
+// https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
+  // console.log(`${URL}`)
 
 function renderGallery(array) {
+  // const pagination = new Pagination(document.getElementById('pagination'), {
+  //       totalItems: 0,
+  //       itemsPerPage: 20,
+  //       visiblePages: 5,
+  //   centerAlign: true,
+  //       page: 1
+  // });
+//   pagination.on('afterMove', (event) => {
+//     const currentPage = event.page;
+//       getImg().then((photo) => {
+    
+//     renderGallery(photo.data)
+   
+//   }).catch(error => error(console.log(error)));
+//      console.log(currentPage);
+// });
+// pagination.on('beforeMove', (event) => {
+//     const currentPage = event.page;
+//   getImg().then((photo) => {
+    
+//     renderGallery(photo.data)
+   
+//   }).catch(error => error(console.log(error)));
+//     if (currentPage === 10) {
+//         return false;
+//         // return true;
+//     }
+// });
+  pagination.reset(array.total_results)
   const cardHeight = refs.gallery.clientHeight
     refs.galleryContainer.insertAdjacentHTML("beforeend", createGallery(array));
 };
 
+ const pagination = new Pagination(document.getElementById('pagination'), {
+        totalItems: 0,
+        itemsPerPage: 20,
+        visiblePages: 5,
+    centerAlign: true,
+        page: 1
+  });
+
+pagination.on('afterMove', (event) => {
+    const currentPage = event.page;
+      getImg().then((photo) => {
+    
+    renderGallery(photo.data)
+   
+  }).catch(error => error(console.log(error)));
+     console.log(currentPage);
+});
+pagination.on('beforeMove', (event) => {
+    const currentPage = event.page;
+  getImg().then((photo) => {
+    
+    renderGallery(photo.data)
+   
+  }).catch(error => error(console.log(error)));
+    if (currentPage === 10) {
+        return false;
+        // return true;
+    }
+});
 
 
 
